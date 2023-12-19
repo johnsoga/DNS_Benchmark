5096623a7c3c5242c60db7180346cb46baf3ecb1
Gary Johnson <1448547+johnsoga@users.noreply.github.com>
2023-12-19 01:50:20 -0500

    Move: Struct Definition for DNS Server

--- dns_types.h
+++ dns_types.h
@@ -1,14 +1,7 @@
-/* DNS HEADER FORMAT
-*
-*
-*
-*
-*/
-
 #ifndef DNS_TYPES_H
 #define DNS_TYPES_H
 
-struct DNS_HEADER {
+typedef struct dnsheader {
 
     // HEADER SIZE IS 12 BYTES
     unsigned short id;          // identification number
@@ -17,6 +10,11 @@ struct DNS_HEADER {
     unsigned short an_count;    // number of resource records in answer
     unsigned short ns_count;    // number of name server resource records in authority records section
     unsigned short ar_count;    // number of resource records in additional records section
-};
+} DNS_HEADER;
+
+typedef struct dnsserver {
+    char *DNSProviderName;
+    char *DNSProviderIP;
+} dnshost;
 
 #endif
